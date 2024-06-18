973fc602dd5e6971b3ced09fdf6216060222c76c
Louis VILLATTE <E225686L@ubuntu.u-inf-j-c004-16>
2024-06-18 11:10:25 +0200

    ajout liste genre et text view pour seekbar

--- SAE_mobile/app/src/main/java/com/example/sae_mobile/MainActivity.kt
+++ SAE_mobile/app/src/main/java/com/example/sae_mobile/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.sae_mobile
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.Spinner
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.example.sae_mobile.data.RecipeDAO
@@ -14,6 +16,8 @@ import io.ktor.client.engine.okhttp.OkHttp
 import io.ktor.client.plugins.HttpTimeout
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var spinner: Spinner
 
     private val viewModel: RecipeViewModel by viewModels{
         val apiService = ApiService(kTorClient)
@@ -29,6 +33,50 @@ class MainActivity : AppCompatActivity() {
 
         val button = findViewById<Button>(R.id.btn_search)
 
+        spinner = findViewById<Spinner>(R.id.dropdawn_genre)
+
+        /*---------------------------------------------------Début Spinner--------------------------------------------------------------*/
+        // Définir la liste des types de cuisines
+        val cuisines = listOf(
+            "",
+            "African",
+            "Asian",
+            "American",
+            "British",
+            "Cajun",
+            "Caribbean",
+            "Chinese",
+            "Eastern European",
+            "European",
+            "French",
+            "German",
+            "Greek",
+            "Indian",
+            "Irish",
+            "Italian",
+            "Japanese",
+            "Jewish",
+            "Korean",
+            "Latin American",
+            "Mediterranean",
+            "Mexican",
+            "Middle Eastern",
+            "Nordic",
+            "Southern",
+            "Spanish",
+            "Thai",
+            "Vietnamese"
+        )
+
+        // Créer un adaptateur pour le Spinner
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, cuisines)
+
+        // Spécifier le layout du dropdown
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        // Assigner l'adaptateur au Spinner
+        spinner.adapter = adapter
+        /*---------------------------------------------------Fin Spinner--------------------------------------------------------------*/
         /*viewModel.recipes.observe(this, { recipes ->
             // Mettre à jour l'interface utilisateur avec les recettes
             println(recipes)
